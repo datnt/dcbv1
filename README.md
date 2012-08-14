d42d7cfb59cbbae0be850ea081b9a90bf3c63570
datnt2 <datnt2@datnt-desktop.(none)>
2012-08-14 09:46:38 +0700

    update

--- README.md
+++
@@ -1,4 +0,0 @@
-dcbv1
-=====
-
-Do Choi Be Vui 1
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dcbv1::Application.config.secret_token = 'ecbf5d179d55cec3f11e0d15fdd53ce818e37708c9ab4e2313e1bcf64d7df6b885b93a20247bb5c07f7f608d9cc30ff9279a62cc2c2cc79db606576fedf3dcc7'
