0dabc434b98d91d437dec84a798ee378ab2df60f
datnt2 <datnt2@datnt-desktop.(none)>
2012-08-15 09:08:17 +0700

    display image with bigger size

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,8 @@ class User < ActiveRecord::Base
 
   has_attached_file :avatar,
                     :storage => :database,
-                    :styles => { :thumb => "75x75>", :small => "150x150>" },
+#                    :styles => { :thumb => "75x75>", :small => "150x150>" },
+                    :styles => { :thumb => "75x75>", :small => "555x417>" },
                     :url => '/:class/:id/:attachment?style=:style'
 
 scope :without_file_data, select_without_file_columns_for(:avatar)
