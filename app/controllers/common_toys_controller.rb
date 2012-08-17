218b521c5a9054ff6514799e52875456df90efe7
datnt2 <datnt2@datnt-desktop.(none)>
2012-08-17 11:49:13 +0700

    perpage 12 item

--- app/controllers/common_toys_controller.rb
+++ app/controllers/common_toys_controller.rb
@@ -1,7 +1,7 @@
 class CommonToysController < ApplicationController
   def index
     #@toys = Toy.find(:all)
-    @toys = Toy.find(:all).paginate({:page => params[:page], :per_page => 2})
+    @toys = Toy.find(:all).paginate({:page => params[:page], :per_page => 12})
   end
 
   def showpage
