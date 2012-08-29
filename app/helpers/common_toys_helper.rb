bd8d8c99d096a4aec83c66bfdeab0198c31f3b39
datnt2 <datnt2@datnt-desktop.(none)>
2012-08-29 09:45:41 +0700

    create related item function

--- app/helpers/common_toys_helper.rb
+++ app/helpers/common_toys_helper.rb
@@ -1,2 +1,15 @@
 module CommonToysHelper
+  def show_related(toy)
+    arr1 = []
+    arr1 = toy.category_obj.toys.map{ |p| p.id}
+
+    arr2 = []
+    arr2 = toy.age_cates.map{ |p| p.id }
+
+    arr2.select {|p| arr1 << p if !arr1.include?(p) }
+
+    @rtoys = Toy.all.select { |c| arr1.include?(c.id) }
+
+    render :partial => "shared/rel_itm"
+  end
 end
