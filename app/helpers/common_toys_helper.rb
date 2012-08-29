8a4c019aa98cdb9e59d055fc920a868230ce0f40
datnt2 <datnt2@datnt-desktop.(none)>
2012-08-29 10:22:00 +0700

    commit to test after rename heroku

--- app/helpers/common_toys_helper.rb
+++ app/helpers/common_toys_helper.rb
@@ -2,10 +2,8 @@ module CommonToysHelper
   def show_related(toy)
     arr1 = []
     arr1 = toy.category_obj.toys.map{ |p| p.id}
-
     arr2 = []
     arr2 = toy.age_cates.map{ |p| p.id }
-
     arr2.select {|p| arr1 << p if !arr1.include?(p) }
 
     if arr1.size > 6
