ca8082f94ed9f07f3a4165c8e91134059da21341
datnt2 <datnt2@datnt-desktop.(none)>
2012-09-21 10:34:23 +0700

    search and display result

--- app/controllers/common_toys_controller.rb
+++ app/controllers/common_toys_controller.rb
@@ -32,6 +32,12 @@ class CommonToysController < ApplicationController
 
   def search
     @search_str = params[:str_search]
+
+    str1='%' + @search_str +'%'
+    str2='%' + @search_str  +'%'
+
+    @toys = Toy.all(:conditions => ["lower(name) LIKE ? OR lower(description) LIKE ?", str1.downcase, str2.downcase])
+
   end
 
   private
