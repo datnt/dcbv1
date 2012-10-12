be7f441995dc4f950d8f714364eec7df9da44003
datnt2 <datnt2@datnt-desktop.(none)>
2012-10-12 16:35:23 +0700

    update thuvien

--- app/controllers/kid_libs_controller.rb
+++ app/controllers/kid_libs_controller.rb
@@ -2,4 +2,11 @@ class KidLibsController < ApplicationController
   def index
     @videos = KidLib.find(:all)
   end
+
+  def show
+    @video = KidLib.find_by_id(params[:id])
+
+    @rel_vids = KidLib.find(:all, :conditions=> ["id not in (?)", [params[:id]]])
+  end
+
 end
