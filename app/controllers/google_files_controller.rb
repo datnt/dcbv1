f1f992555f6637e61f5206d418ae5df2f419022b
Thiện Đạt Ngô <ngothiendat@gmail.com>
2016-04-02 11:49:48 +0700

    Able to upload file to a shared directory and reload the image

--- app/controllers/google_files_controller.rb
+++ app/controllers/google_files_controller.rb
@@ -4,7 +4,74 @@ class GoogleFilesController < ApplicationController
 
 
   def index
-    render :text => "Welcome: <b> Connectivity </b>: #{@connectivity}"
+    list_files
+    # upload_file_to_directory
+    render :text => "Welcome: <b> Connectivity </b>: #{@connectivity}; <img src='https://drive.google.com/uc?export=view&id=0B1fwbuqA93HJQWRrUS0yaEhFb1E'/> "
+  end
+
+  def list_files
+    fres = @client.execute!(
+      :api_method => @client.discovered_api('drive', 'v2').files.list,
+      :parameters => {})
+    puts "inspecting list of files"
+    puts "====="
+    puts "====="
+    puts "====="
+    puts "====="
+    puts "====="
+    # puts "fres' methods == #{fres.methods}"
+    list_data = fres.data
+    # puts "--content of data -- "
+    # puts "list_data == #{list_data.items.size}"
+    puts "LOOP through item of data"
+    list_data.items.each do |item|
+      puts "============***************************"
+      puts "item == #{item.methods}"
+      puts "embed_link === #{item.to_json}"
+    end
+  end
+
+  def upload_file_to_directory
+    drive = @client.discovered_api('drive', 'v2')
+
+    file = drive.files.insert.request_schema.new({
+        :title => "DochoiBeVui.png",
+        :description => "DochoiBeVui.png",
+        :mimeType => "image/jpeg",
+        :parents => [:kind => "drive#file",:id => "0B74OU5RnV3PnbS0xZTNaYVNyQTQ"] #This is the id of directory "appstorage" shared from main account's storage
+      })
+
+
+
+    media = Google::APIClient::UploadIO.new("/Users/dannybrown/Pictures/tigers-25a.jpg", "image/jpeg")
+
+
+    result2 = @client.execute!(
+      :api_method => @client.discovered_api('drive', 'v2').files.insert,
+      :body_object => file,
+      :media => media,
+      :parameters => {
+        'uploadType' => 'multipart',
+        'alt' => 'json'})
+  end
+
+  def upload_files
+    drive = @client.discovered_api('drive', 'v2')
+
+    file = drive.files.insert.request_schema.new({
+        'title' => "rys.jpg",
+        'description' => "hello rys.jpg",
+        'mimeType' => "image/jpeg"
+      })
+    media = Google::APIClient::UploadIO.new("/Users/dannybrown/Pictures/tigers-25a.jpg", "image/jpeg")
+    result = @client.execute!(
+      :api_method => @client.discovered_api('drive', 'v2').files.insert,
+      :body_object => file,
+      :media => media,
+      :parameters => {
+        :parents => "0B9Zmc5gyM-vFRktxWTAzTkc1ZjQ",
+        'uploadType' => 'multipart',
+        'alt' => 'json'})
   end
 
   private
